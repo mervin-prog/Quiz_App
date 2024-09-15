8b1c878ac5c753bedcf8762833194c5854a6282c
mervin <mervinrenie@gmail.com>
2024-09-15 14:04:47 +0530

    changes in script.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/Quiz_App/'
+   base: '/Quiz_App/',
+   build: {
+    outDir: 'build' // Output build files to 'build' directory
+  }
 })
